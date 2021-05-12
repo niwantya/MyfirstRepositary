e07c164150a6f78c3975738f2d581dae46f57f48
Niwant <niwantya@github.com>
2021-05-12 12:24:45 +0530

    don

---
+++ src/codixniwant1/HelloWorld.java
@@ -0,0 +1,9 @@
+package codixniwant1;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/niwant/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.niwant;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");	// TODO Auto-generated method stub
+
+	}
+
+}
